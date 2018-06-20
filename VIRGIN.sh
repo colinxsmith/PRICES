c60723e11838bf39dd4370a248dce8a62236afbf
Colin <colinxsmith@gmail.com>
2018-06-20 22:19:47 +0100

    Where did that VIR come from?

--- VIRGIN.sh
+++ VIRGIN.sh
@@ -4,4 +4,4 @@ now=$((($(date +%s) - 3600*24)))
 yesterday=$(date +%d/%m/%Y --date=@$now)
 yesterday=`echo $yesterday | sed "s|/|\\\\\\\\/|g"`
 echo $yesterday
-curl -A "Mozilla/4.0"  https://uk.virginmoney.com/fundprices/prices.csv | sed -n "/$yesterday/p"  | awk -F, '/UK Index Track/{print $3}' | awk -vDT=$(date +%d/%m/%Y --date=@$now) -vUU=$units '{print DT,$NF,UU,$NF*UU}' VIR
+curl -A "Mozilla/4.0"  https://uk.virginmoney.com/fundprices/prices.csv | sed -n "/$yesterday/p"  | awk -F, '/UK Index Track/{print $3}'|sed "s/\"//g" | awk -vDT=$(date +%d/%m/%Y --date=@$now) -vUU=$units '{print DT,$NF,UU,$NF*UU}'
