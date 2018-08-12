4124313a98a74efe8dfd184ce5671d843c6a8d5e
Colin <colinxsmith@gmail.com>
2018-08-12 16:23:39 +0100

    Don't echo yesterday

--- VIRGIN.sh
+++ VIRGIN.sh
@@ -3,5 +3,5 @@ units=3992.382
 now=$((($(date +%s) - 3600*24)))
 yesterday=$(date +%d/%m/%Y --date=@$now)
 yesterday=`echo $yesterday | sed "s|/|\\\\\\\\/|g"`
-echo $yesterday
+#echo $yesterday
 curl -A "Mozilla/4.0"  https://uk.virginmoney.com/fundprices/prices.csv | sed -n "/$yesterday/p"  | awk -F, '/UK Index Track/{print $3}'|sed "s/\"//g" | awk -vDT=$(date +%d/%m/%Y --date=@$now) -vUU=$units '{print DT,$NF,UU,$NF*UU}'
