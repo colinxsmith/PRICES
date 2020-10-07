e6ca20254d597709522c257b377733f8c8b4fae8
Colin Smith <colinxsmith@gmail.com>
2020-10-07 22:22:20 +0100

    Update VIRGIN.sh

--- VIRGIN.sh
+++ VIRGIN.sh
@@ -7,6 +7,7 @@ units=4374.23
 units=4445.083
 unitsAK=95439.844
 unitsAKb=9311.205
+unitsAKp=12786.471
 now=$((($(date +%s) - 3600*24)))
 yesterday=$(date +%d/%m/%Y --date=@$now)
 yesterday=`echo $yesterday | sed "s|/|\\\\\\\\/|g"`
