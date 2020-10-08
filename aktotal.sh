868e6682e6bec5fe6729bb5539482aaeb69c2e72
Colin <colinxsmith@gmail.com>
2020-10-08 11:51:15 +0100

    .

--- aktotal.sh
+++ aktotal.sh
@@ -2,5 +2,5 @@
 now=$((($(date +%s) - 3600*24)))
 yesterday=$(date +%d/%m/%Y --date=@$now)
 yesterday=`echo $yesterday | sed "s|/|\\\\\\\\/|g"`
-echo $yesterday
-sed -n "/$yesterday/p" histAK | awk 'BEGIN{S=0;};{S+=$4;};END{print "Total: ",S;}'
+#sed -n "/$yesterday/p" histAK | awk 'BEGIN{S=0;};{S+=$4;};END{print "Total: ",S;}'
+cat histAK | awk 'BEGIN{S=0;};{S+=$4;};END{print $1,"Total: ",S;}'
