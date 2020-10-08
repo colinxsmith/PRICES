d67a29a0faf55a1c5930a0fbfa8c4a02cef68364
Colin Smith <colinxsmith@gmail.com>
2020-10-08 17:30:08 +0100

    Update aktotal.sh

--- aktotal.sh
+++ aktotal.sh
@@ -7,4 +7,4 @@ before=$(date +%d/%m/%Y --date=@$before)
 before=`echo $before | sed "s|/|\\\\\\\\/|g"`
 #sed -n "/$before/p" histAK | awk -v b=$before 'BEGIN{S=0;};{S+=$4;};END{print b,"Total: ",S;}'
 #sed -n "/$yesterday/p" histAK | awk -v b=$yesterday 'BEGIN{S=0;};{S+=$4;};END{print b,"Total: ",S;}'
-cat histAK | awk 'BEGIN{S=0;i=0};{{S+=$4;};if(i++%3==2){print $1,"Total: ",S;S=0;}}'
+cat ~/prices/histAK | awk 'BEGIN{S=0;i=0};{{S+=$4;};if(i++%3==2){print $1,"Total: ",S;S=0;}}'
