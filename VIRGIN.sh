c14424ee5168dcd3966bdbc3c120223430411b40
Colin <colinxsmith@gmail.com>
2021-07-05 21:40:04 +0100

    Update for Global Share Fund

--- VIRGIN.sh
+++ VIRGIN.sh
@@ -7,6 +7,7 @@ units=4374.23
 units=4445.083
 units=4499.932
 units=4544.927
+units=8357.198
 unitsAKi=95439.844
 unitsAKb=9311.205
 unitsAKp=12786.471
@@ -18,10 +19,9 @@ yesterday=$(date +%d/%m/%Y --date=@$now)
 yesterday=`echo $yesterday | sed "s|/|\\\\\\\\/|g"`
 #echo $yesterday
 
-
 if [ $person = colin ]
 then
-curl -A "Mozilla/4.0"  https://uk.virginmoney.com/fundprices/prices.csv | sed -n "/$yesterday/p"  | awk -F, '/UK Index Track/{print $3}'|sed "s/\"//g" | awk -vDT=$(date +%d/%m/%Y --date=@$now) -vUU=$units '{print DT,$NF,UU,$NF*UU}'
+curl -A "Mozilla/4.0"  https://uk.virginmoney.com/fundprices/prices.csv | sed -n "/$yesterday/p"  | awk -F, '/\"Global Share Fund/{print $3}'|sed "s/\"//g" | awk -vDT=$(date +%d/%m/%Y --date=@$now) -vUU=$units '{print DT,$NF,UU,$NF*UU}'
   fi
 
 
